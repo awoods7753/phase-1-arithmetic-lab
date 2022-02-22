69f27a57d2648627ff31c6a330109a760efc42c5
Austin <awoods7753@gmail.com>
2022-02-22 16:21:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let multiply = 31 * 2;
+ let num1 = 31;
+ let num2 = 2;
+console.log(num1 * num2);
+
+let random = 1;
+console.log(Math.random(1));
+
+let mod = 22 % 6;
+ let num3 = 22;
+ let num4 = 6;
+console.log(num3 % num4);
+
+let max = Math.max(5, 10, 15, 20);
